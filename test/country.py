b81d0ee4a0c8a10945d545cb1ab0d804d6954a61
cristian <cvazquez@dc.uba.ar>
2017-09-30 00:14:56 -0300

    Add Map object with test

---
+++ test/country.py
@@ -0,0 +1,11 @@
+import unittest
+
+from src.country import Country
+
+
+class TestCountry(unittest.TestCase):
+    def test_create_instance(self):
+        self.assertTrue(Country('xyz'))
+
+    def test_get_name(self):
+        self.assertEqual(Country('Argentina').get_name(), 'Argentina')
