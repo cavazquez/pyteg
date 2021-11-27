a4c570a0ae6af12b65cbf16044b63d237cc9d4a1
Cristian <cristianvazquez4@gmail.com>
2021-11-27 01:16:39 -0300

    socket: first aproch, rename main to server. Create client.py

---
+++ src/client.py
@@ -0,0 +1,17 @@
+import socket
+
+
+def main():
+    host = '127.0.0.1'  # The server's hostname or IP address
+    port = 65432  # The port used by the server
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((host, port))
+        s.sendall(b'Hello, world')
+        data = s.recv(1024)
+
+    print('Received', repr(data))
+
+
+if __name__ == '__main__':
+    main()
