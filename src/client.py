75114fbc683c043841bbb923a7ab1d73fb0267e0
Cristian <cristianvazquez4@gmail.com>
2022-01-23 04:33:08 -0300

    CLient: Clases Transceiver, Connection

--- src/client.py
+++ src/client.py
@@ -14,43 +14,43 @@ class Connection:
     def get_data(self):
         return self._socket.recv(1024)
 
+class Transceiver:
 
-def sender(connection):
-    username = input('username: ')
-    username_data = json.dumps({'username': username})
-    # s.sendall(username_data.encode())
-    connection.send_data(username_data.encode())
-    while True:
-        data = input()
-        json_data = json.dumps({'chat': data})
-        # s.sendall(json_data.encode())
-        connection.send_data(json_data.encode())
+    @staticmethod
+    def receiver(connection):
+        data_b = ""
+        data_json = ""
+        while data_b != b'':
+            data_b =  connection.get_data()
+            if not data_b:
+                print("socket connection broken")
+                continue
 
+            data = data_b.decode()
+            data_json = json.loads(data)
 
-def receiver(connection):
-    data = ""
-    while data != b'':
-        # data_b = s.recv(1024)
-        data_b =  connection.get_data()
-        data = data_b.decode()
-        data_json = json.loads(data)
-        if 'chat' in data_json:
-            print(data_json['chat'])
+            if 'chat' in data_json:
+                print(data_json['chat'])
+
+    @staticmethod
+    def sender(connection):
+        username = input('username: ')
+        username_data = json.dumps({'username': username})
+        # s.sendall(username_data.encode())
+        connection.send_data(username_data.encode())
+        while True:
+            data = input()
+            json_data = json.dumps({'chat': data})
+            # s.sendall(json_data.encode())
+            connection.send_data(json_data.encode())
 
 
 def main():
-    # host = '127.0.0.1'  # The server's hostname or IP address
-    # port = 65432  # The port used by the server
-
-    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # s.connect((host, port))
     connection = Connection()
-    t = threading.Thread(target=receiver, args=[connection])
+    t = threading.Thread(target=Transceiver.receiver, args=[connection])
     t.start()
-    print(t)
-    # t = threading.Thread(target=sender, args=[s])
-    # t.start()
-    sender(connection)
+    Transceiver.sender(connection)
+    t.join()
 
 
 if __name__ == '__main__':
