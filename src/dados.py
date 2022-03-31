5889822fb43a8b70bcec2a74a3ef003b150e71f6
Cristian <cristianvazquez4@gmail.com>
2022-03-31 14:03:20 -0300

    Dados: Ahora son de 6 caras

--- src/dados.py
+++ src/dados.py
@@ -5,8 +5,8 @@ class Dados:
 
     @staticmethod
     def tirar_dados(cant):
-        return choices(range(1, 6), k=cant)
+        return choices(range(1, 7), k=cant)
 
     @staticmethod
     def tirar_dados_ordenados(cant):
-        return sorted(choices(range(1, 6), k=cant), reverse=True)
+        return sorted(choices(range(1, 7), k=cant), reverse=True)
