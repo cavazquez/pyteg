c9cd2d737dc1ff07a4b8873162165000727dd68c
Cristian <cristianvazquez4@gmail.com>
2022-07-08 02:37:19 -0300

    2 columnas agregadas al GUI

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QRadioButton, QVBoxLayout, QLineEdit, QGridLayout
 from PySide6.QtCore import QSize
 
 # Only needed for access to command line arguments
@@ -7,22 +7,50 @@ import sys
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
 # If you know you won't use command line arguments QApplication([]) works too.
-app = QApplication(sys.argv)
 
-# Create a Qt widget, which will be our window.
-#window = QWidget()
-window = QMainWindow()
-window.setWindowTitle("PyTeg")
-button = QPushButton("Push me")
-window.setCentralWidget(button)
+class Gui(QMainWindow):
 
-#window.setFixedSize(QSize(800, 600))
-window.setFixedSize(QSize(1024, 768))
+    def __init__(self):
+        QMainWindow.__init__(self, parent=None)
+        #window = QMainWindow()
+        self.setWindowTitle("PyTeg")
 
-window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
-# Start the event loop.
+        VMap = QVBoxLayout()
+        test_jugador = QLineEdit()
+        VMap.addWidget(test_jugador)
+
+        Vlayout = QVBoxLayout()
+        self._jugadores = []
+        for i in range(0,6):
+            jugador = QLineEdit()
+            jugador.setReadOnly(True)
+            self._jugadores.append(jugador)
+            Vlayout.addWidget(jugador)
+
+        grid_layout = QGridLayout()
+        grid_layout.addLayout(VMap, 0 , 0)
+        grid_layout.addLayout(Vlayout, 0 , 1)
+        grid_layout.setColumnStretch(0, 4)
+        grid_layout.setColumnStretch(1, 1)
+
+        container = QWidget()
+        container.setLayout(grid_layout)
+
+        self.setCentralWidget(container)
+
+        #window.setFixedSize(QSize(800, 600))
+        self.setFixedSize(QSize(1024, 768))
+
+        self.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
+    def update(self, text):
+        self._jugadores[0].insert(text)
+
+
+app = QApplication()
+gui = Gui()
+gui.update("Hola")
+
 app.exec()
 
-# Your application won't reach here until you exit and the event
-# loop has stopped.
