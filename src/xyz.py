2ae3aae58a0a5589c653c6695bb6bb0e85e5d4b5
Cristian <cristianvazquez4@gmail.com>
2023-06-16 23:20:57 -0300

    paises.toml: Consulta de paises y coordenadas

--- src/xyz.py
+++ src/xyz.py
@@ -7,26 +7,33 @@ class XYZ:
             toml_string = f.read()
             self.parsed_toml = tomllib.loads(toml_string)
         self.continentes = {}
+        self.paises = {}
         self.cartas = self.parsed_toml["Cartas"]
         del self.parsed_toml["Cartas"]
 
         for continente in self.parsed_toml:
             datos  = self.parsed_toml[continente]
-            print(datos)
-            self.continentes[continente] = (datos['pos_x'])
+            self.continentes[continente] = (datos.get('pos_x'), datos.get('pos_y'))
+            del datos['pos_x']
+            del datos['pos_y']
+            for k in datos:
+                self.paises[k] = datos.get(k)
 
-    def paises(self, continente):
-        return [k for k in self.parsed_toml]
+    def paises(self):
+        return [k for k in self.paises]
 
-    def coordenadas(self, pais, continente):
-        p = self.parsed_toml[continente][pais]
+    def coordenadas_continente(self, continente):
+        return self.continentes[continente]
+
+    def coordenadas(self, pais):
+        p = self.paises[pais]
         return p["pos_x"], p["pos_y"], p["army_x"], p["army_y"]
 
     def get_cartas(self):
-        return self.parsed_toml['Cartas']
+        return self.cartas
 
-    def img_path(self, pais, continente):
-        return self.parsed_toml[pais]["file"]
+    def img_path(self, pais):
+        return self.paises[pais]["file"]
 
-    def continente(self, pais, continente):
-        return self.parsed_toml[pais]["continente"]
+    def continente(self, pais):
+        return self.paises[pais]["continente"]
