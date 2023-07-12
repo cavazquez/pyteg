3b69b48c388481bf7db3e85ee1e5dc048a5f3dde
Cristian <cristianvazquez4@gmail.com>
2023-07-12 02:38:57 -0300

    test: Rondas Coverage 100%

--- src/rondas.py
+++ src/rondas.py
@@ -3,61 +3,65 @@ from src.turnos import SiguientesTurnos, SegundoTurno, PrimerTurno
 
 
 class SiguientesRondas:
-    def __proximo_turno(self, jugadores, mapa):
-        for turno in [SiguientesTurnos(id_jugador, mapa) for id_jugador in jugadores]:
-            yield turno
 
-    def __init__(self, jugadores, game):
-        print("Siguientes ronda")
+    def __init__(self, jugadores, mapa):
         self._unidades = {}
         self._jugadores = jugadores
-        self._turnos = self.__proximo_turno(jugadores, game.mapa())
-        self._turno_actual = next(self._turnos)
-        self._game = game
+        self._turnos = [SiguientesTurnos(id_jugador, mapa) for id_jugador in jugadores]
+        self._turno_actual = self.turnos()[0]
+        self._num_turno_actual = 0
 
     def usar_unidad(self):
         self._turno_actual.usar_unidad()
 
+    def jugadores(self):
+        return self._jugadores
+
+    def turnos(self):
+        return self._turnos
+
     def turno_actual(self):
         return self._turno_actual
 
+
     def finalizar_turno(self):
         try:
-            self._turno_actual = next(self._turnos)
-        except StopIteration:
-            self._game._ronda = SiguientesRondas(self._jugadores, self._game)
+            self._num_turno_actual += 1
+            self._turno_actual = self.turnos()[self._num_turno_actual]
+        except IndexError:
+            self._turno_actual = None
 
 
 class SegundaRonda:
-    def __proximo_turno(self, jugadores):
-        for turno in [SegundoTurno(id_jugador) for id_jugador in jugadores]:
-            yield turno
 
-    def __init__(self, jugadores, game):
-        print("Segunda ronda")
+    def __init__(self, jugadores):
         self._unidades = {}
         self._jugadores = jugadores
-        self._turnos = self.__proximo_turno(jugadores)
-        self._turno_actual = next(self._turnos)
-        self._game = game
+        self._turnos = [SegundoTurno(id_jugador) for id_jugador in jugadores]
+        self._turno_actual = self._turnos[0]
+        self._num_turno_actual = 0
 
     def usar_unidad(self):
         self._turno_actual.usar_unidad()
+
+    def jugadores(self):
+        return self._jugadores
+
+    def turnos(self):
+        return self._turnos
 
     def turno_actual(self):
         return self._turno_actual
 
     def finalizar_turno(self):
         try:
-            self._turno_actual = next(self._turnos)
-        except StopIteration:
-            self._game._ronda = SiguientesRondas(self._jugadores, self._game)
+            self._num_turno_actual += 1
+            self._turno_actual = self.turnos()[self._num_turno_actual]
+        except IndexError:
+            self._turno_actual = None
 
 
 class PrimeraRonda:
-    #def __proximo_turno(self, jugadores):
-    #    for turno in [PrimerTurno(id_jugador) for id_jugador in jugadores]:
-    #        yield turno
 
     def __init__(self, jugadores):
         self._jugadores = jugadores
--- tests/test_rondas.py
+++ tests/test_rondas.py
@@ -1,8 +1,8 @@
 
 import unittest
 
-from src.rondas import PrimeraRonda
-#from src.mapa import Mapa
+from src.rondas import PrimeraRonda, SegundaRonda, SiguientesRondas
+from src.mapa import Mapa
 
 class TestPrimeraRonda(unittest.TestCase):
 
@@ -29,5 +29,88 @@ class TestPrimeraRonda(unittest.TestCase):
         ronda.finalizar_turno()
         ronda.finalizar_turno()
         self.assertEqual(ronda.turno_actual(), None)
+
+    def test_usar_unidad(self):
+        ronda = PrimeraRonda(["Fulano","Mengano"])
+        ronda.usar_unidad()
+
+class TestSegundaRonda(unittest.TestCase):
+
+    def test_jugadores(self):
+        ronda = SegundaRonda(["Fulano","Mengano"])
+        self.assertListEqual(ronda.jugadores(), ["Fulano","Mengano"])
+
+    def test_cantidad_turnos(self):
+        ronda = SegundaRonda(["Fulano","Mengano"])
+        self.assertEqual(len(ronda.turnos()), 2)
+
+    def test_turno_actual(self):
+        ronda = SegundaRonda(["Fulano","Mengano"])
+        self.assertEqual(ronda.turno_actual(), ronda.turnos()[0])
+
+    def test_finalizar_turno(self):
+        ronda = SegundaRonda(["Fulano","Mengano"])
+        ronda.finalizar_turno()
+        self.assertEqual(ronda.turno_actual(), ronda.turnos()[1])
+
+
+    def test_sin_turnos(self):
+        ronda = SegundaRonda(["Fulano","Mengano"])
+        ronda.finalizar_turno()
+        ronda.finalizar_turno()
+        self.assertEqual(ronda.turno_actual(), None)
+
+    def test_usar_unidad(self):
+        ronda = SegundaRonda(["Fulano","Mengano"])
+        ronda.usar_unidad()
+
+
+class TestSiguientesRondas(unittest.TestCase):
+
+    def test_jugadores(self):
+        def build_mapa():
+            return {}
+        mapa = Mapa(build_mapa)
+        ronda = SiguientesRondas(["Fulano","Mengano"], mapa)
+        self.assertListEqual(ronda.jugadores(), ["Fulano","Mengano"])
+
+    def test_cantidad_turnos(self):
+        def build_mapa():
+            return {}
+        mapa = Mapa(build_mapa)
+        ronda = SiguientesRondas(["Fulano","Mengano"], mapa)
+        self.assertEqual(len(ronda.turnos()), 2)
+
+    def test_turno_actual(self):
+        def build_mapa():
+            return {}
+        mapa = Mapa(build_mapa)
+        ronda = SiguientesRondas(["Fulano","Mengano"], mapa)
+        self.assertEqual(ronda.turno_actual(), ronda.turnos()[0])
+
+    def test_finalizar_turno(self):
+        def build_mapa():
+            return {}
+        mapa = Mapa(build_mapa)
+        ronda = SiguientesRondas(["Fulano","Mengano"], mapa)
+        ronda.finalizar_turno()
+        self.assertEqual(ronda.turno_actual(), ronda.turnos()[1])
+
+
+    def test_sin_turnos(self):
+        def build_mapa():
+            return {}
+        mapa = Mapa(build_mapa)
+        ronda = SiguientesRondas(["Fulano","Mengano"], mapa)
+        ronda.finalizar_turno()
+        ronda.finalizar_turno()
+        self.assertEqual(ronda.turno_actual(), None)
+
+    def test_usar_unidad(self):
+        def build_mapa():
+            return {}
+        mapa = Mapa(build_mapa)
+        ronda = SiguientesRondas(["Fulano","Mengano"], mapa)
+        ronda.usar_unidad()
 
 
