bad7ec5b2386339963670d63a91cbe96d77fec39
Cristian <cristianvazquez4@gmail.com>
2023-07-12 01:55:28 -0300

    test: Turnos Coverage 100%

--- src/turnos.py
+++ src/turnos.py
@@ -7,10 +7,10 @@ class SiguientesTurnos:
         self._unidades = Calculos.calcular_unidades_generales(mapa, jugador)
         self._unidades_europa = Calculos.calcular_unidades_europa(mapa, jugador)
         self._unidades_africa = Calculos.calcular_unidades_africa(mapa, jugador)
-        self._unidades_america_del_sur = Calculos.calcular_unidades_america_del_sur(
+        self._unidades_sudamerica = Calculos.calcular_unidades_america_del_sur(
             mapa, jugador
         )
-        self._unidades_america_del_norte = Calculos.calcular_unidades_america_del_norte(
+        self._unidades_norteamerica = Calculos.calcular_unidades_america_del_norte(
             mapa, jugador
         )
         self._unidades_asia = Calculos.calcular_unidades_asia(mapa, jugador)
@@ -22,6 +22,44 @@ class SiguientesTurnos:
     def usar_unidad(self):
         self._unidades -= 1
 
+    def usar_unidad_africa(self):
+        self._unidades_africa -= 1
+
+    def usar_unidad_europa(self):
+        self._unidades_europa -= 1
+
+    def usar_unidad_oceania(self):
+        self._unidades_oceania -= 1
+
+    def usar_unidad_asia(self):
+        self._unidades_asia -= 1
+
+    def usar_unidad_sudamerica(self):
+        self._unidades_sudamerica -= 1
+
+    def usar_unidad_norteamerica(self):
+        self._unidades_norteamerica -= 1
+
+    def cant_unidades(self):
+        return self._unidades
+
+    def cant_unidades_africa(self):
+        return self._unidades_africa
+
+    def cant_unidades_europa(self):
+        return self._unidades_europa
+
+    def cant_unidades_oceania(self):
+        return self._unidades_oceania
+
+    def cant_unidades_asia(self):
+        return self._unidades_asia
+
+    def cant_unidades_sudamerica(self):
+        return self._unidades_sudamerica
+
+    def cant_unidades_norteamerica(self):
+        return self._unidades_norteamerica
 
 class SegundoTurno:
     def __init__(self, jugador):
@@ -34,6 +72,9 @@ class SegundoTurno:
     def usar_unidad(self):
         self._unidades -= 1
 
+    def cant_unidades(self):
+        return self._unidades
+
 
 class PrimerTurno:
     def __init__(self, jugador):
@@ -45,3 +86,6 @@ class PrimerTurno:
 
     def usar_unidad(self):
         self._unidades -= 1
+
+    def cant_unidades(self):
+        return self._unidades
---
+++ tests/test_turnos.py
@@ -0,0 +1,142 @@
+import unittest
+
+from src.turnos import PrimerTurno, SegundoTurno, SiguientesTurnos
+from src.mapa import Mapa
+
+class TestPrimerTurno(unittest.TestCase):
+
+    def test_jugador_actual(self):
+        primer_turno = PrimerTurno('Fulano')
+        self.assertEqual(primer_turno.jugador_actual(), 'Fulano')
+
+    def test_cant_unidades(self):
+        primer_turno = PrimerTurno('Fulano')
+        self.assertEqual(primer_turno.cant_unidades(), 6)
+
+    def test_usar_unidad(self):
+        primer_turno = PrimerTurno('Fulano')
+        primer_turno.usar_unidad()
+        self.assertEqual(primer_turno.cant_unidades(), 5)
+
+class TestSegundoTurno(unittest.TestCase):
+
+    def test_jugador_actual(self):
+        segundo_turno = SegundoTurno('Fulano')
+        self.assertEqual(segundo_turno.jugador_actual(), 'Fulano')
+
+    def test_cant_unidades(self):
+        segundo_turno = SegundoTurno('Fulano')
+        self.assertEqual(segundo_turno.cant_unidades(), 3)
+
+    def test_usar_unidad(self):
+        segundo_turno = SegundoTurno('Fulano')
+        segundo_turno.usar_unidad()
+        self.assertEqual(segundo_turno.cant_unidades(), 2)
+
+class TestSiguientesTurnos(unittest.TestCase):
+
+    def test_jugador_actual(self):
+        def build_mapa():
+            return {'Argentina': [5, 'Africa', 'Mengano'],
+                    'Uruguay': [10, 'Africa', 'Fulano'], 
+                    'Chile': [1, 'America', None]}
+        mapa = Mapa(build_mapa)
+        siguientes_turnos = SiguientesTurnos('Fulano', mapa)
+        self.assertEqual(siguientes_turnos.jugador_actual(), 'Fulano')
+
+    def test_cant_unidades(self):
+        def build_mapa():
+            return {'Argentina': [5, 'Africa', 'Mengano'],
+                    'Uruguay': [10, 'Africa', 'Fulano'], 
+                    'Chile': [1, 'America', None]}
+        mapa = Mapa(build_mapa)
+        siguientes_turnos = SiguientesTurnos('Fulano', mapa)
+        self.assertEqual(siguientes_turnos.cant_unidades(), 3)
+
+    def test_usar_unidad(self):
+        def build_mapa():
+            return {'Argentina': [5, 'Africa', 'Mengano'],
+                    'Uruguay': [10, 'Africa', 'Fulano'], 
+                    'Chile': [1, 'America', None]}
+        mapa = Mapa(build_mapa)
+        siguientes_turnos = SiguientesTurnos('Fulano', mapa)
+        siguientes_turnos.usar_unidad()
+        self.assertEqual(siguientes_turnos.cant_unidades(), 2)
+
+
+    def test_usar_unidad_toda_africa_en_africa(self):
+        def build_mapa():
+            return {'Argentina': [5, 'Africa', 'Fulano'],
+                    'Uruguay': [10, 'Africa', 'Fulano'], 
+                    'Chile': [1, 'America', None]}
+        mapa = Mapa(build_mapa)
+        siguientes_turnos = SiguientesTurnos('Fulano', mapa)
+        unidades = siguientes_turnos.cant_unidades_africa()
+        siguientes_turnos.usar_unidad_africa()
+        self.assertEqual(
+                siguientes_turnos.cant_unidades_africa(), unidades - 1)
+
+
+    def test_usar_unidad_toda_europa_en_europa(self):
+        def build_mapa():
+            return {'Argentina': [5, 'Europa', 'Fulano'],
+                    'Uruguay': [10, 'Europa', 'Fulano'], 
+                    'Chile': [1, 'America', None]}
+        mapa = Mapa(build_mapa)
+        siguientes_turnos = SiguientesTurnos('Fulano', mapa)
+        unidades = siguientes_turnos.cant_unidades_europa()
+        siguientes_turnos.usar_unidad_europa()
+        self.assertEqual(
+                siguientes_turnos.cant_unidades_europa(), unidades - 1)
+
+
+    def test_usar_unidad_toda_oceania_en_oceania(self):
+        def build_mapa():
+            return {'Argentina': [5, 'Oceania', 'Fulano'],
+                    'Uruguay': [10, 'Oceania', 'Fulano'], 
+                    'Chile': [1, 'America', None]}
+        mapa = Mapa(build_mapa)
+        siguientes_turnos = SiguientesTurnos('Fulano', mapa)
+        unidades = siguientes_turnos.cant_unidades_oceania()
+        siguientes_turnos.usar_unidad_oceania()
+        self.assertEqual(
+                siguientes_turnos.cant_unidades_oceania(), unidades - 1)
+
+
+    def test_usar_unidad_toda_asia_en_asia(self):
+        def build_mapa():
+            return {'Argentina': [5, 'Asia', 'Fulano'],
+                    'Uruguay': [10, 'Asia', 'Fulano'], 
+                    'Chile': [1, 'America', None]}
+        mapa = Mapa(build_mapa)
+        siguientes_turnos = SiguientesTurnos('Fulano', mapa)
+        unidades = siguientes_turnos.cant_unidades_asia()
+        siguientes_turnos.usar_unidad_asia()
+        self.assertEqual(
+                siguientes_turnos.cant_unidades_asia(), unidades - 1)
+
+
+    def test_usar_unidad_toda_sudamerica_en_sudamerica(self):
+        def build_mapa():
+            return {'Argentina': [5, 'Sudamerica', 'Fulano'],
+                    'Uruguay': [10, 'Sudamerica', 'Fulano'], 
+                    'Chile': [1, 'America', None]}
+        mapa = Mapa(build_mapa)
+        siguientes_turnos = SiguientesTurnos('Fulano', mapa)
+        unidades = siguientes_turnos.cant_unidades_sudamerica()
+        siguientes_turnos.usar_unidad_sudamerica()
+        self.assertEqual(
+                siguientes_turnos.cant_unidades_sudamerica(), unidades - 1)
+
+
+    def test_usar_unidad_toda_norteamerica_en_norteamerica(self):
+        def build_mapa():
+            return {'Argentina': [5, 'Norteamerica', 'Fulano'],
+                    'Uruguay': [10, 'Norteamerica', 'Fulano'], 
+                    'Chile': [1, 'America', None]}
+        mapa = Mapa(build_mapa)
+        siguientes_turnos = SiguientesTurnos('Fulano', mapa)
+        unidades = siguientes_turnos.cant_unidades_norteamerica()
+        siguientes_turnos.usar_unidad_norteamerica()
+        self.assertEqual(
+                siguientes_turnos.cant_unidades_norteamerica(), unidades - 1)
