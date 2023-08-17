57f4ba7924b5aaf92b81ef93575d5f97760f5239
Cristian <cristianvazquez4@gmail.com>
2023-08-17 20:53:58 -0300

    refactor: tarjeta de pais deja de ser una dataclass

--- src/tarjeta_de_pais.py
+++ src/tarjeta_de_pais.py
@@ -1,13 +1,14 @@
-from dataclasses import dataclass
 
 
-@dataclass(frozen=True)
 class TarjetaDePais:
-    pais: str
-    simbolo: str
+
+    def __init__(self, pais, simbolo):
+        self.pais = pais
+        self.simbolo = simbolo
 
     def dame_pais(self):
         return self.pais
 
     def dame_simbolo(self):
         return self.simbolo
+
