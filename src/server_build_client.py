f37a77e9feaaf7ea4fda37cfdd3517778b6486e0
Cristian <cvazquez@dc.uba.ar>
2024-07-31 00:37:28 -0300

    feat: nueva clase ServerBuildClient

---
+++ src/server_build_client.py
@@ -0,0 +1,17 @@
+from itertools import cycle
+
+from src.server_client import Client
+
+
+class ServerBuildClient:
+
+    def __init__(self):
+        self._user_id = 1
+        self._username = cycle["Cortazar", "Borges", "Sabato", "Arlt", "Bioy", "Saer"]
+
+    def build(self, connection, server):
+        username = next(self._username)
+        user_id = self._user_id
+        self._user_id += 1
+        client = Client(user_id, connection, server, username)
+        return user_id, client
