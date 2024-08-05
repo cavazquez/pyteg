e5b83bd1f643c3843f9cf06fce7f27b1c6aa95f0
Cristian <cvazquez@dc.uba.ar>
2024-08-05 13:20:59 -0300

    feat: En Esperando Jugadores agrego 6 radio button en 2 columnas

--- src/gui_esperar_jugadores.py
+++ src/gui_esperar_jugadores.py
@@ -1,4 +1,6 @@
 from PySide6.QtWidgets import (
+    QGridLayout,
+    QRadioButton,
     QVBoxLayout,
     QWidget,
 )
@@ -9,5 +11,21 @@ class VentanaEsperarJugadores(QWidget):
     def __init__(self, main_window):
         super().__init__()
         self._main_window = main_window
+        self.inicializar_ui()
+
+    def inicializar_ui(self):
+        self.setWindowTitle("Esperando jugadores")
+
         layout = QVBoxLayout()
+
+        grid_layout = QGridLayout()
+
+        # Crear 6 botones como opciones
+        for i in range(6):
+            radio = QRadioButton(f"Opción {i}", self)
+            fila = i // 2
+            columna = i % 2
+            grid_layout.addWidget(radio, fila, columna)
+
+        layout.addLayout(grid_layout)
         self.setLayout(layout)
