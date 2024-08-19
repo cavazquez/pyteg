08622613d34e8f1ec1daf7baafa0f31e4c4fb1a9
Cristian <cvazquez@dc.uba.ar>
2024-08-19 00:12:40 -0300

    feat: Cuando está esperando jugadores los RadioButton se muestran con el color que se le asigna al jugador

--- src/cliente_colores.py
+++ src/cliente_colores.py
@@ -1,19 +1,25 @@
 class Colores:
 
     def __init__(self):
-        self.colores = []
-        self.asignacion = {}
+        self._colores = []
+        self._asignacion = {}
 
     def agregar_color(self, color):
-        self.colores.append(color)
+        self._colores.append(color)
 
     def asignar(self, cliente, color):
-        self.asignacion[cliente] = color
+        self._asignacion[cliente] = color
+
+    def colores(self):
+        return self._colores
+
+    def colores_asignados(self):
+        return self._asignacion
 
     def __str__(self):
-        colores = ", ".join(str(color) for color in self.colores)
+        colores = ", ".join(str(color) for color in self._colores)
         asignacion = "\n".join(
-            f"{clave}: {valor}" for clave, valor in self.asignacion.items()
+            f"{clave}: {valor}" for clave, valor in self._asignacion.items()
         )
         return f"""Colores:
         {colores}
--- src/gui_esperar_jugadores.py
+++ src/gui_esperar_jugadores.py
@@ -21,11 +21,17 @@ class VentanaEsperarJugadores(QWidget):
         grid_layout = QGridLayout()
 
         # Crear 6 botones como opciones
+        colores = self._main_window.colores.colores()
         for i in range(6):
-            radio = QRadioButton(f"Opción {i}", self)
+            color = colores[i]
+            radio = QRadioButton("    ", self)
+            radio.setStyleSheet(f"background-color: {color.name()};")
             fila = i // 2
             columna = i % 2
             grid_layout.addWidget(radio, fila, columna)
+
+        colores_asignados = self._main_window.colores.colores_asignados()
+        print(colores_asignados)
 
         layout.addLayout(grid_layout)
         self.setLayout(layout)
