feea62cef14bb73235bbad46aa319f000620733b
Cristian <cvazquez@dc.uba.ar>
2025-05-22 02:01:18 -0300

    feat: botones para conectar, atacar y mover

--- src/gui_toolbar.py
+++ src/gui_toolbar.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QAction
+from PySide6.QtGui import QAction, QIcon
 from PySide6.QtWidgets import QToolBar
 
 
@@ -10,6 +10,14 @@ class ToolBar(QToolBar):
         self.setFloatable(False)
         self.setAllowedAreas(Qt.TopToolBarArea)
 
-        button_conectar = QAction("Conectar", self)
+        button_conectar = QAction(QIcon("icons/conectar.png"), "Conectar", self)
         button_conectar.triggered.connect(main_window.abrir_ventana_conectar)
         self.addAction(button_conectar)
+
+        button_atacar = QAction(QIcon("icons/atacar.png"), "Atacar", self)
+        #button_atacar.triggered.connect(main_window.abrir_ventana_atacar)
+        self.addAction(button_atacar)
+
+        button_mover = QAction(QIcon("icons/mover.png"), "Mover", self)
+        #button_mover.triggered.connect(main_window.abrir_ventana_mover)
+        self.addAction(button_mover)
