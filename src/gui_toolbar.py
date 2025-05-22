be4268ad8b0e6ce0f7a7dc6f76cb3dad985abdb4
Cristian <cvazquez@dc.uba.ar>
2025-05-22 02:05:52 -0300

    feat: Los botones del toolbar ahora estan centrados

--- src/gui_toolbar.py
+++ src/gui_toolbar.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction, QIcon
-from PySide6.QtWidgets import QToolBar
+from PySide6.QtWidgets import QSizePolicy, QToolBar, QWidget
 
 
 class ToolBar(QToolBar):
@@ -9,6 +9,11 @@ class ToolBar(QToolBar):
         self.setMovable(False)
         self.setFloatable(False)
         self.setAllowedAreas(Qt.TopToolBarArea)
+
+        # Expansor izquierdo
+        left_spacer = QWidget(self)
+        left_spacer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        self.addWidget(left_spacer)
 
         button_conectar = QAction(QIcon("icons/conectar.png"), "Conectar", self)
         button_conectar.triggered.connect(main_window.abrir_ventana_conectar)
@@ -21,3 +26,8 @@ class ToolBar(QToolBar):
         button_mover = QAction(QIcon("icons/mover.png"), "Mover", self)
         # button_mover.triggered.connect(main_window.abrir_ventana_mover)
         self.addAction(button_mover)
+
+        # Expansor derecho
+        right_spacer = QWidget(self)
+        right_spacer.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        self.addWidget(right_spacer)
